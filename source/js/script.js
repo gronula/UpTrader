f77fbea37ac89885c92c50fb3d50371e95a94320
gronula <gronula@yandex.ru>
2018-12-21 08:24:59 +0300

    Добавляет стилизацию десктопной версии

--- source/js/script.js
+++ source/js/script.js
@@ -2,34 +2,72 @@
 
 (function () {
   var page = document.body;
-  var tab = document.querySelectorAll('.tabs__type');
-  var header = document.querySelectorAll('.tabs__header');
-  var info = document.querySelectorAll('.tabs__info');
-  var button = document.querySelectorAll('.tabs__button');
+  var pageHeader = document.querySelector('.header');
+  var headers = document.querySelectorAll('.header__button');
+  var main = document.querySelector('.main');
+  var tabs = document.querySelectorAll('.tabs__type');
+  var tabHeaders = document.querySelectorAll('.tabs__header');
+  var tabTitles = document.querySelectorAll('.tabs__title');
+  var tabInfos = document.querySelectorAll('.tabs__info');
+  var buttons = document.querySelectorAll('.tabs__button');
 
-  var tabsClickHandler = function (element, title, message) {
-    title.addEventListener('click', function () {
-      if (window.matchMedia('(max-width: 768px)').matches) {
+  var tabsHeaderClickHandler = function (element, header, message) {
+    header.addEventListener('click', function () {
+      if (window.matchMedia('(max-width: 1199px)').matches) {
         if (!message.classList.contains('tabs__info--opened')) {
-          page.classList.add('page--opened');
+
           element.classList.add('tabs__type--opened');
-          title.classList.add('tabs__header--opened');
+          header.classList.add('tabs__header--opened');
           message.classList.add('tabs__info--opened');
         } else {
           page.classList.remove('page--opened');
           element.classList.remove('tabs__type--opened');
-          title.classList.remove('tabs__header--opened');
+          header.classList.remove('tabs__header--opened');
           message.classList.remove('tabs__info--opened');
         }
 
-        button.forEach(function (it) {
+        buttons.forEach(function (it) {
           it.textContent = 'Открыть счет';
         });
       }
     })
   };
 
-  for (var i = 0; i < tab.length; i++) {
-    tabsClickHandler(tab[i], header[i], info[i]);
+  for (var i = 0; i < tabs.length; i++) {
+    tabsHeaderClickHandler(tabs[i], tabHeaders[i], tabInfos[i]);
+  }
+
+  var headerClickHandler = function (header, title) {
+    header.addEventListener('click', function (evt) {
+      page.classList.add('page--opened');
+      pageHeader.classList.add('header--closed');
+      main.classList.add('main--opened');
+      for (var i = 0; i < headers.length; i++) {
+        if (evt.target === headers[i]) {
+          tabTitles[i].classList.add('tabs__title--active');
+          tabInfos[i].classList.add('tabs__info--opened');
+        } else {
+          tabTitles[i].classList.remove('tabs__title--active');
+          tabInfos[i].classList.remove('tabs__info--opened');
+        }
+      }
+    });
+    title.addEventListener('click', function (evt) {
+      for (var i = 0; i < headers.length; i++) {
+        if (evt.target === tabTitles[i]) {
+          tabTitles[i].classList.add('tabs__title--active');
+          tabInfos[i].classList.add('tabs__info--opened');
+        } else {
+          tabTitles[i].classList.remove('tabs__title--active');
+          tabInfos[i].classList.remove('tabs__info--opened');
+        }
+      }
+    });
+  };
+
+  if (window.matchMedia('(min-width: 1200px)').matches) {
+    for (var i = 0; i < tabs.length; i++) {
+      headerClickHandler(headers[i], tabTitles[i]);
+    }
   }
 })();
